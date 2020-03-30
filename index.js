8ac316497540ba052e6114265c4d1d565b7e5950
Sooraj Parameswaran <srjpn@gmail.com>
2020-03-30 18:48:57 +0530

    Simple server to list google drive files

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { google } = require('googleapis');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => res.end('Enlighto'));
+
+app.get('/list', async (req, res) => {
+    const drive = google.drive({
+        version: 'v3',
+        auth: process.env.API_KEY
+    });
+
+    drive.files.list({
+        q: `'${process.env.PARENT_DIR_ID}' in parents`,
+    }).then(({ data }) => res.json(data.files.map(x => ({ name: x.name, url: `http://drive.google.com/uc?export=view&id=${x.id}` }))))
+        .catch(err => res.status(400).send(err.message));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`listening to ${PORT}...`);
+});
